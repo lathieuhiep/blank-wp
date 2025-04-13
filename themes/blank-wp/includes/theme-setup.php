18143d88e534c7c31b9f80fc8db284717ecaa818
lathieuhiep <khacdiepkma90@gmail.com>
2025-04-13 23:37:11 +0700

    update core theme WordPress

---
+++ themes/blank-wp/includes/theme-setup.php
@@ -0,0 +1,52 @@
+<?php
+if ( ! function_exists( 'blank_wp_setup' ) ) :
+    function blank_wp_setup(): void
+    {
+
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
+
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support( 'title-tag' );
+
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support( 'post-thumbnails' );
+
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(
+            array(
+                'primary' => esc_html__( 'Primary Menu', 'blank-wp' ),
+            )
+        );
+
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            )
+        );
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support( 'customize-selective-refresh-widgets' );
+    }
+endif;
+add_action( 'after_setup_theme', 'blank_wp_setup' );
---
+++ themes/blank-wp/includes/theme-sidebar.php
@@ -0,0 +1,16 @@
+<?php
+function blank_wp_widgets_init(): void
+{
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Sidebar', 'blank-wp' ),
+            'id'            => 'sidebar-1',
+            'description'   => esc_html__( 'Add widgets here to appear in your sidebar.', 'blank-wp' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action( 'widgets_init', 'blank_wp_widgets_init' );
