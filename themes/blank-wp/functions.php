73cf7a812e430abae567ffaa98c19b3e86600a9e
diep.lk <diep.lk@kiotviet.com>
2025-04-14 15:53:59 +0700

    update core theme

--- themes/blank-wp/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-require get_parent_theme_file_path( '/includes/theme-setup.php' );
-require get_parent_theme_file_path( '/includes/theme-hooks.php' );
-require get_parent_theme_file_path( '/includes/theme-sidebar.php' );
-require get_parent_theme_file_path( '/includes/theme-functions.php' );
