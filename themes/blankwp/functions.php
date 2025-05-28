7b2b51a71f74bf4f59bd747ce53b8ef3a71b5276
diep.lk <diep.lk@kiotviet.com>
2025-05-28 10:48:36 +0700

    update core theme

--- themes/blankwp/functions.php
+++ themes/blankwp/functions.php
@@ -3,6 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+
 // Required: Plugin Activation
 require get_parent_theme_file_path( '/includes/class-tgm-plugin-activation.php' );
 require get_parent_theme_file_path( '/includes/plugin-activation.php' );
@@ -13,3 +14,5 @@ require get_parent_theme_file_path( '/includes/theme-scripts.php' );
 require get_parent_theme_file_path( '/includes/theme-hooks.php' );
 require get_parent_theme_file_path( '/includes/theme-sidebar.php' );
 require get_parent_theme_file_path( '/includes/theme-functions.php' );
+
+require get_parent_theme_file_path( '/includes/meta-boxes/page-mb.php' );
